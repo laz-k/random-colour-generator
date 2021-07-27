bae5c0095b4d19550aa367d0391ec9a0b734ec0c
laz-k <l.kloukas@yahoo.com>
2021-07-27 07:11:41 +0100

    Initialization

---
+++ src/Components/ColourBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ColourBox extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ src/Components/ColourBoxGenerator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ColourBox from './ColourBox'
+
+export default class ColourBoxGenerator extends Component {
+  render() {
+    return (
+      <div>
+        <ColourBox/>
+      </div>
+    )
+  }
+}
